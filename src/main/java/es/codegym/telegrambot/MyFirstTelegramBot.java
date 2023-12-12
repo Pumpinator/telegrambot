d0708c97ccc3d43439b7052b90e56fa2385ba8ff
Pumpinator <alejandrodcardona5@gmail.com>
2023-12-11 22:07:48 -0600

    Bot Greetings

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -7,8 +7,8 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
-    public static final String NAME = "agregue el nombre del bot entre comillas a continuación";
-    public static final String TOKEN = "agregue el token del bot entre comillas a continuación";
+    public static final String NAME = "PumpinatorBot_bot";
+    public static final String TOKEN = "6762709434:AAGIGWU5gU3qQq9bLCdMd1dSwNIxREnFJDw";
 
     public MyFirstTelegramBot() {
         super(NAME, TOKEN);
@@ -16,7 +16,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
     @Override
     public void onUpdateEventReceived(Update update) {
-        // TODO: escribiremos la funcionalidad principal del bot aquí
+        sendTextMessageAsync("¡Hola, amante de las pesas y los fierros!");
     }
 
     public static void main(String[] args) throws TelegramApiException {
